3438856ccfc611b08d1dc482ba0b0cc7f3d3c9fa
Nlarseny <larsen.d.nathan@gmail.com>
2022-01-10 16:19:56 -0700

    first rough workiing hourglass program

--- HourGlass/hourglass.py
+++ HourGlass/hourglass.py
@@ -7,9 +7,95 @@ import dns.query
 import dns.flags
 
 
+class TimeStamps:
+    def __init__(self, hour = 0, min = 0, sec = 0):
+        self.hour = hour
+        self.min = min
+        self.sec = sec
+
+    def print_time(self):
+        line = str(self.hour) + ":" + str(self.min) + ":" + str(self.sec)
+        print(line)
+
+    def to_seconds(self):
+        return (self.hour * 3600) + (self.min * 60) + self.sec
+
+
+def createTimeStamp():
+    x = datetime.now().time()
+
+    hour = x.strftime("%H")
+    minute = x.strftime("%M")
+    second = x.strftime("%S")
+
+    result = TimeStamps(int(hour), int(minute), int(second))
+
+    return result
+
+
+# current, target to get time till
+def deltaTimeStamp(time_a, time_b):
+    total_a_seconds = time_a.to_seconds()
+    total_b_seconds = time_b.to_seconds()
+
+    # z = y - x
+    return total_b_seconds - total_a_seconds
+
+
+def checkIfTime(time_a, time_b, flex_max, flex_min):
+    delta = deltaTimeStamp(time_a, time_b)
+
+    if delta >= flex_min and delta <= flex_max:
+        return True
+    else:
+        return False
+
+
+def get_list_times(file_name):
+    my_file = open(file_name, "r")
+    content = my_file.readlines()
+    return content
+
+
+def next_target(time_list, current_time):
+    # print(time_list)
+    times = []
+    for i in time_list:
+        x = str(i)
+        x = x.strip()
+        #print(x)
+        result = x.split(":")
+        final = TimeStamps(int(result[0]), int(result[1]), float(result[2]))
+
+        times.append(final)
+
+    # current_time.print_time()
+    time_till = []
+    for t in times:
+        delta = deltaTimeStamp(current_time, t)
+        # t.print_time()
+        # print(delta, "!!!")
+        time_till.append(delta)
+
+    smallest = 99999999999999
+    iter = -1
+    small_iter = -1
+    for t in time_till:
+        iter += 1
+        print(iter)
+        if t >= 0:
+            if t < smallest:
+                smallest = t
+                small_iter = iter
+
+    # print(small_iter, smallest, time_till[small_iter])
+    #times[small_iter].print_time()
+    return times[small_iter]
+
+
 def get_serial(target, server_root):
-    #domain = '199.7.91.13'
-    #name_server = '8.8.8.8' # @ part of dig
+    #domain = '199.7.91.13' aka the target
+    #name_server = '8.8.8.8' aka server_root # @ part of dig
     ADDITIONAL_RDCLASS = 65535
 
     domain = dns.name.from_text(target)
@@ -30,16 +116,12 @@ def get_serial(target, server_root):
     return -1
 
 
-def get_list_times(file_name):
-    my_file = open(file_name, "r")
-    content = my_file.read()
-    return content
-
-
 def main(argv):
     opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
 
     # get list of times from file
+    #ip_addr = args[1]
+    # exampel: dig @127.0.0.1 example.com121223 SOA
     list_of_times = get_list_times(args[0])
     
 
@@ -50,18 +132,19 @@ def main(argv):
 
     while 1:
         # where we need to feed difference between times
+        current_time = createTimeStamp()
+        
+        # create next target time
+        target_time = next_target(list_of_times, current_time)
 
-        results = stamp_parser("22:17:44.262345")
-        
-        target_time = datetime.now().time()
-        temp = datetime.strptime("21:34:01", "%H:%M:%S").time()
-        print(type(target_time))
-        
-        target_time = target_time.replace(hour = temp.hour, minute = temp.minute, second = temp.second)
-        
-        print("target time", target_time)
-        timer_call(target, target_time, 0, 1, 5)
-        time.sleep(1)
+        result_check = checkIfTime(current_time, target_time, 12889, 0)
+        if result_check:
+            print(get_serial(target, "8.8.8.8"))
+            print("HA")
+        else:
+            print("BWAHAHA")
+
+        time.sleep(5)
 
 
 
