646186fb6170a8265d357d76a663a55286caa261
Nate Larsen <nlarse17@imaal10.cs.byu.edu>
2022-01-20 15:20:33 -0700

    forced

--- Atlas/experiment_api.py
+++ Atlas/experiment_api.py
@@ -4,7 +4,8 @@ import requests
 # The probes will be selected around the world to see how long it takes for the root servers to update
 
 # https://atlas.ripe.net:443/api/v2/measurements/{measurement_id}/results/?probe_ids=8756
-
+x = {'amount':'33', 'recipient':'larsen.d.nathan@gmail.com', }
+requests.post("https://atlas.ripe.net:443/api/v2/credits/transfers", data=x)
 # /api/v2/measurements/34861161/results/?format=json
 response = requests.get("https://atlas.ripe.net:443/api/v2/measurements/dns/")
 #print(response.content()) # Return the raw bytes of the data payload
