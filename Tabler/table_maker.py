0ac6223da2875a6ae2db694f63f274c2a9e0f786
Nlarseny <larsen.d.nathan@gmail.com>
2022-02-03 13:39:04 -0700

    tabler can make a valid table now

--- Tabler/table_maker.py
+++ Tabler/table_maker.py
@@ -6,6 +6,32 @@ import csv
 from csv import writer
 
 
+class TimeStamps:
+    def __init__(self, hour = 0, min = 0, sec = 0):
+        self.hour = hour
+        self.min = min
+        self.sec = sec
+
+    def print_time(self):
+        line = str(self.hour) + ":" + str(self.min) + ":" + str(self.sec)
+        print(line)
+
+    def to_seconds(self):
+        return (self.hour * 3600) + (self.min * 60) + self.sec
+
+    def get_time(self):
+        line = str(self.hour) + ":" + str(self.min) + ":" + str(self.sec)
+        return line
+
+
+def deltaTimeStamp(time_a, time_b):
+    total_a_seconds = time_a.to_seconds()
+    total_b_seconds = time_b.to_seconds()
+
+    # z = y - x
+    return total_b_seconds - total_a_seconds
+
+
 def get_filenames():
     file_list = glob.glob("./test/*.txt") # Include slash or it will search in the wrong directory
 
@@ -14,35 +40,66 @@ def get_filenames():
     return file_list
 
 
-def make_row():
+def make_rows():
     files = get_filenames()
 
+
+    # do while there are lines, all files should have the same number of lines after being cleaned
+    line_num = 0
+    with open(files[0]) as f:
+        lines = f.readlines()
+        line_num = len(lines)
+
     rows = []
-    flag = 0
-    row_iter = 0
-    for file in files:
-        with open(file) as f:
+    for i in range(0, line_num):
+        # get ith element for each file
+        row = []
+        # get serial for the row
+        with open(files[0]) as f:
             lines = f.readlines()
-            
-            tupled = []
-            for line in lines:
-                tupled.append(line.split())
+            temp = lines[i].split()
+            row.append(temp[1])
+
+        for file in files:
+            with open(file) as f:
+                lines = f.readlines()
                 
-            row = []
-            for tup in tupled: 
-                if flag == 0:
-                    row.append(tup[1])
-                    flag = 1
-                row.append(tup[0])
+                tupled = []
+                for line in lines:
+                    tupled.append(line.split())
+                row.append(tupled[i][0])
+        
+        rows.append(row)
+
+    return rows
             
             
+def get_spread(rows):
+    # need a winner by row
+
+    # this converts the rows to rows of timestamp objects
+    for i in range(0, len(rows)):
+        for j in range (1, len(rows[i])):
+            result = rows[i][j].split(":")
+            print(rows[i][j])
+            final = TimeStamps(int(result[0]), int(result[1]), float(result[2]))
+            rows[i][j] = final
+
+
+    
 
 
 def create_table(file_list):
-    list_of_headers = ["", "verisign(a)", "USC", "CogentCom",
-    "UM", "NASA", "ISC", "US DD", "Army", "Netnod", "verisign(j)", "RIPE", "ICANN", "WIDE"]
+    # list_of_headers = ["SERIALS", "verisign(a)", "USC", "CogentCom",
+    # "UM", "NASA", "ISC", "US DD", "Army", "Netnod", "verisign(j)", "RIPE", "ICANN", "WIDE"]
 
-    make_row()
+    list_of_headers = ["SERIALS"]
+    files = get_filenames()
+    for file in files:
+        temp = file.split('.')
+        file_name = temp[1]
+        list_of_headers.append(file_name)
+
 
     # Open file in append mode
     with open('practice.csv', 'a+', newline='') as write_obj:
@@ -51,9 +108,12 @@ def create_table(file_list):
         # Add headers
         csv_writer.writerow(list_of_headers)
 
-    # with open("practice.csv", 'a') as csv_file:
-    #     first = str(datetime.now().time()) + " " + str(current_serial) + "\n"
-    #     csv_file.write(first)
+        rows = make_rows()
+        for row in rows:
+            csv_writer.writerow(row)
+
+        # get the spread for each row
+        get_spread(rows)
     
 
 def main(argv):
